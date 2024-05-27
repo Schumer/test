0b67cf46d795d63672284359a76358f896cae79a
Schumer <danielschumacher82@gmail.com>
2024-05-27 10:44:58 +0200

    Add files via upload

---
+++ script27.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('fillButton').addEventListener('click', function() {
+    const progressBar = document.querySelector('.progress');
+    progressBar.style.width = '100%';
+});
