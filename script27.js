a50443efc901f96f7babe2e127ce4a47f8ee35d9
Schumer <danielschumacher82@gmail.com>
2024-05-27 11:06:15 +0200

    Update script27.js

--- script27.js
+++ script27.js
@@ -2,12 +2,15 @@
 let progress = 0;
 
 document.getElementById('fillButton').addEventListener('click', function() {
-    console.log('Button clicked'); // Debugging
     if (progress < 100) {
         progress += 25;
         console.log('Progress:', progress); // Debugging
         const progressBar = document.querySelector('.progress');
         progressBar.style.width = progress + '%';
         console.log('ProgressBar width:', progressBar.style.width); // Debugging
+
+        if (progress === 100) {
+            document.getElementById('star').style.display = 'block';
+        }
     }
 });
